6ff45bd8968579ebb5b0e2814f763050f2250e0b
Parami <paramiashinsana@gmail.com>
2023-08-20 23:18:22 +0530

    create login form and register form

---
+++ src/main/java/lk/ijse/controller/StudentLoginFormController.java
@@ -0,0 +1,19 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
+
+public class StudentLoginFormController {
+    public PasswordField txtPassword;
+    public JFXComboBox cBoxUserProfile;
+
+    public void BtnLoginOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void RegisterOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+}
---
+++ src/main/java/lk/ijse/controller/StudentRegisterFormController.java
@@ -0,0 +1,19 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
+
+public class StudentRegisterFormController {
+    public PasswordField txtPassword;
+    public JFXTextField txtUser;
+
+    public void BtnRegisterOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void backOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+}
