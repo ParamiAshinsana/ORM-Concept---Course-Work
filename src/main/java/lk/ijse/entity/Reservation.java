eb8d42223b030770f732475147dd91c3f95a328e
Parami <paramiashinsana@gmail.com>
2023-08-22 12:22:29 +0530

    create the reservation entity

--- src/main/java/lk/ijse/entity/Reservation.java
+++ src/main/java/lk/ijse/entity/Reservation.java
@@ -1,4 +1,49 @@
 package lk.ijse.entity;
 
 public class Reservation {
+    private String reservationId;
+    private String date;
+    private String status;
+
+    public Reservation() {
+    }
+
+    public Reservation(String reservationId, String date, String status) {
+        this.reservationId = reservationId;
+        this.date = date;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "reservationId='" + reservationId + '\'' +
+                ", date='" + date + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    public String getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(String reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
