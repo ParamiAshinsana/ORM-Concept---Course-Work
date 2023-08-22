1db479b0561e44fb53daf2f7edaaa94fa1a20bf7
Parami <paramiashinsana@gmail.com>
2023-08-22 12:23:57 +0530

    create the room entity

--- src/main/java/lk/ijse/entity/Room.java
+++ src/main/java/lk/ijse/entity/Room.java
@@ -1,4 +1,60 @@
 package lk.ijse.entity;
 
 public class Room {
+    private String roomTypeId;
+    private String type;
+    private Double keyMoney;
+    private int qty;
+
+    public Room() {
+    }
+
+    public Room(String roomTypeId, String type, Double keyMoney, int qty) {
+        this.roomTypeId = roomTypeId;
+        this.type = type;
+        this.keyMoney = keyMoney;
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomTypeId='" + roomTypeId + '\'' +
+                ", type='" + type + '\'' +
+                ", keyMoney=" + keyMoney +
+                ", qty=" + qty +
+                '}';
+    }
+
+    public String getRoomTypeId() {
+        return roomTypeId;
+    }
+
+    public void setRoomTypeId(String roomTypeId) {
+        this.roomTypeId = roomTypeId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getKeyMoney() {
+        return keyMoney;
+    }
+
+    public void setKeyMoney(Double keyMoney) {
+        this.keyMoney = keyMoney;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
 }
