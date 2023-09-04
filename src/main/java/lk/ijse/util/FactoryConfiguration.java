679f1a53c99aabdd287b4c229e2361935bcaa5e9
Parami <paramiashinsana@gmail.com>
2023-09-04 15:14:52 +0530

    Add hibernate property configuration

--- src/main/java/lk/ijse/util/FactoryConfiguration.java
+++ src/main/java/lk/ijse/util/FactoryConfiguration.java
@@ -1,20 +1,31 @@
 package lk.ijse.util;
 
+import lk.ijse.entity.Reservation;
+import lk.ijse.entity.Room;
 import lk.ijse.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration ;
 
     private SessionFactory sessionFactory;
 
-    private FactoryConfiguration(){
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+    private FactoryConfiguration() throws IOException {
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+        configuration.setProperties(properties);
+        configuration.addAnnotatedClass(Student.class);
+        configuration.addAnnotatedClass(Room.class);
+        configuration.addAnnotatedClass(Reservation.class);
         sessionFactory = configuration.buildSessionFactory();
     }
-    public static FactoryConfiguration getInstance(){
+    public static FactoryConfiguration getInstance() throws IOException {
         return (factoryConfiguration==null)? factoryConfiguration=new FactoryConfiguration():factoryConfiguration;
     }
     // returned session
