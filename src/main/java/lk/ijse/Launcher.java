a7322964041dc9e25954c3cbb2d7522c2c464d24
Parami <paramiashinsana@gmail.com>
2023-09-04 20:10:59 +0530

    Create the loading

--- src/main/java/lk/ijse/Launcher.java
+++ src/main/java/lk/ijse/Launcher.java
@@ -2,18 +2,30 @@ package lk.ijse;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("/view/student_login_form.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login Form");
+//        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("/view/student_login_form.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+//        stage.setTitle("Login Form");
+//        stage.setScene(scene);
+//        stage.show();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loading_form.fxml"));
+        stage.centerOnScreen();
+        Scene scene = new Scene(root,600,400, Color.TRANSPARENT);
+        root.setStyle("-fx-background-color: transparent");
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
+
     }
 }
---
+++ src/main/java/lk/ijse/controller/LoadingFormController.java
@@ -0,0 +1,52 @@
+package lk.ijse.controller;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadingFormController implements Initializable {
+
+    public AnchorPane loadingFormRoot;
+    public ImageView loadingImageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadingImageView.setImage(new Image("/img/loading.png"));
+        loadingImageView.setCache(true);
+
+        Timeline timeline = new Timeline();
+        //KeyFrame keyFrame = new KeyFrame(Duration.millis(10000), actionEvent -> {
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(8000), actionEvent -> {
+            System.out.println("Initializing...");
+        });
+
+        timeline.getKeyFrames().addAll(keyFrame);
+        timeline.playFromStart();
+
+        timeline.setOnFinished(actionEvent -> {
+            try{
+                loadingFormRoot.getScene().getWindow().hide();
+                Parent root = FXMLLoader.load(getClass().getResource("/view/student_login_form.fxml"));
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.initStyle(StageStyle.UNDECORATED);
+                stage.show();
+            }catch (IOException e){
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
