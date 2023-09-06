27ff9748809b386e4958a0598bd0e53e10d25f17
Parami <paramiashinsana@gmail.com>
2023-09-06 17:13:18 +0530

    create the Room's Combo Box

--- src/main/java/lk/ijse/controller/RoomsFormController.java
+++ src/main/java/lk/ijse/controller/RoomsFormController.java
@@ -1,16 +1,23 @@
 package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.dto.tm.RoomTM;
 import lk.ijse.dto.tm.StudentTM;
 
-public class RoomsFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RoomsFormController implements Initializable {
     public AnchorPane rootRoom;
     public ComboBox typeCBox;
     public ComboBox roomIdCBox;
@@ -22,11 +29,31 @@ public class RoomsFormController {
     public Button btnClear;
     public Button btnDelete;
 
-    public TableView roomTbl;
+    public TableView <RoomTM>roomTbl;
     public TableColumn <? , ?>colRId;
     public TableColumn <? , ?> colType;
     public TableColumn <? , ?> colKeymoney;
     public TableColumn <? , ?> colQty;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setCellValueFactory();
+        getAll();
+
+        ObservableList<String> roomIdBox = FXCollections.observableArrayList( "RM-1324","RM-5467","RM-7896","RM-0093");
+        roomIdCBox.setItems(roomIdBox);
+
+        ObservableList<String> roomTypeBox = FXCollections.observableArrayList( "Non-AC","Non-AC / Food","AC","AC / Food");
+        typeCBox.setItems(roomTypeBox);
+    }
+
+    private void getAll() {
+
+    }
+
+    private void setCellValueFactory() {
+
+    }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
 
