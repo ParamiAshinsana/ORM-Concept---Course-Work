fc4a3d4ba4b39a52db7ae0dbaa43aff5014e2f5f
Parami <paramiashinsana@gmail.com>
2023-09-07 12:46:37 +0530

    update the ReservationDAO

---
+++ src/main/java/lk/ijse/dao/Custom/ReservationDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.dao.Custom;
+
+import lk.ijse.dao.CrudDAO;
+import lk.ijse.entity.Reservation;
+import lk.ijse.entity.Room;
+
+public interface ReservationDAO extends CrudDAO<Reservation> {
+}
