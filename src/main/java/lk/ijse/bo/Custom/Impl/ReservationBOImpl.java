e54d1477263374cd984eb33347d21978f363ddb6
Parami <paramiashinsana@gmail.com>
2023-09-07 22:07:23 +0530

    Update the ReservationBOImpl

--- src/main/java/lk/ijse/bo/Custom/Impl/ReservationBOImpl.java
+++ src/main/java/lk/ijse/bo/Custom/Impl/ReservationBOImpl.java
@@ -2,13 +2,18 @@ package lk.ijse.bo.Custom.Impl;
 
 import lk.ijse.bo.Custom.ReservationBO;
 import lk.ijse.bo.Custom.RoomBO;
+import lk.ijse.dao.Custom.ReservationDAO;
+import lk.ijse.dao.DAOFactory;
 import lk.ijse.dto.ReservationDTO;
+import lk.ijse.entity.Reservation;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
 public class ReservationBOImpl implements ReservationBO{
+    ReservationDAO reservationDAO = (ReservationDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.D_RESERVATIONS);
+
     @Override
     public List<ReservationDTO> getAllReservations() throws SQLException, ClassNotFoundException, IOException {
         return null;
@@ -16,7 +21,7 @@ public class ReservationBOImpl implements ReservationBO{
 
     @Override
     public boolean addReservations(ReservationDTO dto) throws SQLException, ClassNotFoundException, IOException {
-        return false;
+        return reservationDAO.add(new Reservation(dto.getReservationId(),dto.getStudent(),dto.getRoom(),dto.getDate(),dto.getStatus()));
     }
 
     @Override
