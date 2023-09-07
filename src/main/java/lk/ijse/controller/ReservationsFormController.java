4549db02e6f05178162d4f7174ee266a43d7142a
Parami <paramiashinsana@gmail.com>
2023-09-07 20:19:15 +0530

    create the Reservation's Combo boxes

--- src/main/java/lk/ijse/controller/ReservationsFormController.java
+++ src/main/java/lk/ijse/controller/ReservationsFormController.java
@@ -1,20 +1,37 @@
 package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.Custom.ReservationBO;
 import lk.ijse.bo.Custom.RoomBO;
+import lk.ijse.dao.Custom.ReservationDAO;
+import lk.ijse.dao.Custom.RoomDAO;
+import lk.ijse.dao.DAOFactory;
+import lk.ijse.dto.tm.ReservationTM;
+import lk.ijse.entity.Room;
+import lk.ijse.entity.Student;
+import lombok.SneakyThrows;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ReservationsFormController implements Initializable {
-    RoomBO roomBO = (RoomBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.B_ROOM);
-    String rmId;
+    ReservationBO reservationBO = (ReservationBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.B_RESERVATION);
+    ReservationDAO reservationDAO = (ReservationDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.D_RESERVATIONS);
+    RoomDAO roomDAO = (RoomDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.D_ROOMS);
+    String RsID;
+
+    Student student = new Student();
+    Room room = new Room();
 
     public AnchorPane rootReservation;
     public JFXTextField txtResvId;
@@ -35,9 +52,31 @@ public class ReservationsFormController implements Initializable {
     public TableColumn <? , ?>colDate;
     public TableColumn <? , ?>colStatus;
 
+    @SneakyThrows
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        uploadStudentId();
+        uploadRoomTypeId();
+    }
 
+    private void uploadStudentId() throws IOException {
+        List<String> RSid = reservationDAO.uploadStID();
+        ObservableList<String> rSObservableList = FXCollections.observableArrayList();
+
+        for (String x : RSid){
+            rSObservableList.add(x);
+        }
+        RstIdCBox.setItems(rSObservableList);
+    }
+
+    private void uploadRoomTypeId() throws IOException {
+        List<String> RRid = reservationDAO.uploadRmID();
+        ObservableList<String> rRObservableList = FXCollections.observableArrayList();
+
+        for (String y : RRid){
+            rRObservableList.add(y);
+        }
+        RrmIdCBox.setItems(rRObservableList);
     }
 
     public void RstIdCBoxOnAction(ActionEvent actionEvent) {
