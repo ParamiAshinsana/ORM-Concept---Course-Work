ec22548aaa95bb4e743cd9d7e8e8c59d0287cdc0
Parami <paramiashinsana@gmail.com>
2023-09-07 11:40:17 +0530

    Update the Reservation

--- src/main/java/lk/ijse/controller/ReservationsFormController.java
+++ src/main/java/lk/ijse/controller/ReservationsFormController.java
@@ -1,7 +1,70 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.Custom.RoomBO;
 
-public class ReservationsFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReservationsFormController implements Initializable {
+    RoomBO roomBO = (RoomBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.B_ROOM);
+    String rmId;
+
     public AnchorPane rootReservation;
+    public JFXTextField txtResvId;
+    public ComboBox RstIdCBox;
+    public ComboBox RrmIdCBox;
+    public DatePicker resDatePicker;
+    public ComboBox resStatusCBox;
+
+    public Button btnSave;
+    public Button btnUpdate;
+    public Button btnClear;
+    public Button btnDelete;
+
+    public TableView resvTbl;
+    public TableColumn <? , ?>colResId;
+    public TableColumn <? , ?>colRStId;
+    public TableColumn <? , ?>colRRmId;
+    public TableColumn <? , ?>colDate;
+    public TableColumn <? , ?>colStatus;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void RstIdCBoxOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void RrmIdCBoxOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void resvTblOnClicked(MouseEvent mouseEvent) {
+
+    }
 }
