584ee7ed3508e92046112ae126cc4ed6f85b7898
Parami <paramiashinsana@gmail.com>
2023-09-08 22:31:21 +0530

    Create the User form

--- src/main/java/lk/ijse/controller/HomeFormController.java
+++ src/main/java/lk/ijse/controller/HomeFormController.java
@@ -1,7 +1,48 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class HomeFormController {
     public AnchorPane rootHome;
+    public JFXTextField txtUsName;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtpassword;
+
+    public Button btnSave;
+    public Button btnUpdate;
+    public Button btnClear;
+    public Button btnDelete;
+
+    public TableView userTbl;
+    public TableColumn colUsName;
+    public TableColumn colPassword;
+    public TableColumn colEmail;
+
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void userTblOnClicked(MouseEvent mouseEvent) {
+
+    }
 }
