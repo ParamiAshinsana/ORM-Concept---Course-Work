0395bf4153253130da3276d22b948e55527a45c3
Parami <paramiashinsana@gmail.com>
2023-09-08 23:24:22 +0530

    Create the UserTM

---
+++ src/main/java/lk/ijse/dto/tm/UserTM.java
@@ -0,0 +1,60 @@
+package lk.ijse.dto.tm;
+
+public class UserTM {
+    private String usId;
+    private String usName;
+    private String usPassword;
+    private String usEmail;
+
+    public UserTM() {
+    }
+
+    public UserTM(String usId, String usName, String usPassword, String usEmail) {
+        this.usId = usId;
+        this.usName = usName;
+        this.usPassword = usPassword;
+        this.usEmail = usEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "UserTM{" +
+                "usId='" + usId + '\'' +
+                ", usName='" + usName + '\'' +
+                ", usPassword='" + usPassword + '\'' +
+                ", usEmail='" + usEmail + '\'' +
+                '}';
+    }
+
+    public String getUsId() {
+        return usId;
+    }
+
+    public void setUsId(String usId) {
+        this.usId = usId;
+    }
+
+    public String getUsName() {
+        return usName;
+    }
+
+    public void setUsName(String usName) {
+        this.usName = usName;
+    }
+
+    public String getUsPassword() {
+        return usPassword;
+    }
+
+    public void setUsPassword(String usPassword) {
+        this.usPassword = usPassword;
+    }
+
+    public String getUsEmail() {
+        return usEmail;
+    }
+
+    public void setUsEmail(String usEmail) {
+        this.usEmail = usEmail;
+    }
+}
