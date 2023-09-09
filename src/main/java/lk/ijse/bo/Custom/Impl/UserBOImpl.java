a5e49995322938120492d63353897357af85aff9
Parami <paramiashinsana@gmail.com>
2023-09-09 21:55:01 +0530

    Update UserBOImpl

--- src/main/java/lk/ijse/bo/Custom/Impl/UserBOImpl.java
+++ src/main/java/lk/ijse/bo/Custom/Impl/UserBOImpl.java
@@ -1,13 +1,23 @@
 package lk.ijse.bo.Custom.Impl;
 
 import lk.ijse.bo.Custom.UserBO;
+import lk.ijse.dao.Custom.StudentDAO;
+import lk.ijse.dao.Custom.UserDAO;
+import lk.ijse.dao.DAOFactory;
 import lk.ijse.dto.UserDTO;
+import lk.ijse.entity.Reservation;
+import lk.ijse.entity.Student;
+import lk.ijse.entity.User;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserBOImpl implements UserBO {
+    UserDAO userDAO = (UserDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.D_USERS);
+
+
     @Override
     public List<UserDTO> getAllUsers() throws SQLException, ClassNotFoundException, IOException {
         return null;
@@ -15,7 +25,7 @@ public class UserBOImpl implements UserBO {
 
     @Override
     public boolean addUsers(UserDTO dto) throws SQLException, ClassNotFoundException, IOException {
-        return false;
+        return userDAO.add(new User(dto.getUsId(),dto.getUsName(),dto.getUsPassword(),dto.getUsEmail()));
     }
 
     @Override
@@ -29,7 +39,7 @@ public class UserBOImpl implements UserBO {
     }
 
     @Override
-    public String generateNewUsersID() throws SQLException, ClassNotFoundException {
-        return null;
+    public String generateNewUsersID() throws SQLException, ClassNotFoundException, IOException {
+        return userDAO.generateNewId();
     }
 }
