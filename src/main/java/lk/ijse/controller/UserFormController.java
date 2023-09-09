6bda761e960a29ab2b6ec1a751c20e982e4ff89d
Parami <paramiashinsana@gmail.com>
2023-09-09 21:53:45 +0530

    Update UserFormController

--- src/main/java/lk/ijse/controller/UserFormController.java
+++ src/main/java/lk/ijse/controller/UserFormController.java
@@ -3,18 +3,32 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.Custom.StudentBO;
+import lk.ijse.bo.Custom.UserBO;
+import lombok.SneakyThrows;
 
-public class UserFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class UserFormController implements Initializable {
+    UserBO userBO = (UserBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.B_USER);
+
+
     public AnchorPane rootHome;
     public JFXTextField txtUserID;
     public JFXTextField txtUsName;
     public JFXTextField txtEmail;
     public JFXPasswordField txtpassword;
+    public JFXTextField userIdTxt;
 
     public Button btnSave;
     public Button btnUpdate;
@@ -46,5 +60,20 @@ public class UserFormController {
 
     public void userTblOnClicked(MouseEvent mouseEvent) {
 
+    }
+
+    @SneakyThrows
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        generateNextUserId();
+    }
+
+    private void generateNextUserId() throws ClassNotFoundException {
+        try {
+            String nextId = userBO.generateNewUsersID();
+            userIdTxt.setText(nextId);
+        } catch (SQLException | IOException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
