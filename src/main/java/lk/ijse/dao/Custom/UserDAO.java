323c6e6d82ac6343cb886425489f02cb96c69623
Parami <paramiashinsana@gmail.com>
2023-09-09 21:54:35 +0530

    Update UserDAO

--- src/main/java/lk/ijse/dao/Custom/UserDAO.java
+++ src/main/java/lk/ijse/dao/Custom/UserDAO.java
@@ -3,5 +3,9 @@ package lk.ijse.dao.Custom;
 import lk.ijse.dao.CrudDAO;
 import lk.ijse.entity.User;
 
+import java.io.IOException;
+
 public interface UserDAO extends CrudDAO<User> {
+    boolean checkPassword(String username, String password) throws IOException;
+
 }
